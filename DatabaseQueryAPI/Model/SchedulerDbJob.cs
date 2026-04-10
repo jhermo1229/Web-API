13a0832dca79e88eb32f89c944ea6ebd8a2f90ea
jhermo <jbhermo@yahoo.com>
2026-04-10 10:31:47 -0400

    Added class file

---
+++ DatabaseQueryAPI/Model/SchedulerDbJob.cs
@@ -0,0 +1,13 @@
+﻿namespace DatabaseQueryAPI.Model
+{
+    public class SchedulerDbJob
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public bool Enabled { get; set; }
+        public string TimeOfDay { get; set; } = "";
+        public int PlantId { get; set; }
+        public List<string> DaysOfWeek { get; set; } = new();
+        public List<string> Recipients { get; set; } = new();
+    }
+}
